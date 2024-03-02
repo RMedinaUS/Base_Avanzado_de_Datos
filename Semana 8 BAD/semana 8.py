808e6e82e56113668bdd5ac116f3c8baa00e4a13
Ricardo Medina <123226737+RMedinaUS@users.noreply.github.com>
2024-03-02 13:46:49 -0600

    Correccion de codigo

--- Semana 8 BAD/semana 8.py
+++ Semana 8 BAD/semana 8.py
@@ -1,5 +1,5 @@
 # Importar las librerías
-import filecmp
+import filecamp
 import json
 import mysql.connector
 import pandas as pd
@@ -12,33 +12,71 @@ conn = mysql.connector.connect(
     database="diccionario_db",
 )
 
-# Crear un cursor
+# Variabl de consulta a la base de datos
 cursor = conn.cursor()
 
 # Cargar datos del archivo JSON
 with open("diccionario_maya.json", 'r' encoding="utf-8") as file:
     diccionario_data = json.load(file)
 
-# Recorrer el diccionario y insertar datos en la base de datos
 for palabra in diccionario_data.keys():
     maya = maya
     español = palabra[español]
-
-    # Insertar la palabra en la base de datos
     insert_query = "INSERT INTO diccionario_json (maya, español) VALUES (%s, %s)"
     cursor.execute(insert_query, (maya, español))
 
-# Confirmar los cambios
 conn.commit()
-
-cursor = conn.cursor()
-cursor.execute("SELECT maya, espanol from diccionario_json")
-resultados = cursor.fetchall()
-
-# Cerrar el cursor y la conexión
 cursor.close()
 conn.close()
 
+import mysql.connector
+import pandas as pd
 
+#Conectar a la base de datos
+conn = ysql.connector.connect(
+    host="localhest",
+    user="root",
+    password="root",
+    database="diccionario_db"
+)
 
+# Variable de consulta a la base de datos
+cursor conn.cursor()
 
+# Cargar datos desde el archivo XLSX
+df_excel pd.read_excel('diccionario_maya.xlsx')
+
+for index, row is df_excel.iterrows():
+  maya row ('Maya')
+  espanol row ['Español']
+  insert query "INSERT INTO diccionario xlsx (Maya, espanol) VALUES(%s,%s)"
+  cursor.execute(insert query, (maya, espanol))
+
+conn.commit()
+cursor.close()
+conn.close()
+
+import mysql.connector
+import pandas as pd
+
+# Conectar a la base de datos
+conn = mysql.connector.connect(
+    host= "localhost",
+    user= "root".
+    password ="root",
+    database="diccionario_db"
+)
+
+#Variable de consulta a la base de datos
+cursor conn.cursor()
+
+cursor.execute("SELECT maya, espanol FROM diccionario_json")
+
+resultados = cursor.fetchall()
+
+for fila in resultados:
+    maya, espanol = fila
+    print("Maya: (maya), ----- Español: (espanol)")
+
+cursor.close()
+conn.close()
